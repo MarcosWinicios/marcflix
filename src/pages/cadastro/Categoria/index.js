f3811e028ca3ccef936edf23e12d4c1e26f297e9
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2020-07-31 17:28:38 -0300

    Init form of categories

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -7,6 +7,16 @@ function CadastroCategoria(){
     return(
       <PageDefault>
           <h1>Página de Cadastro de Categorias</h1>
+          <form>
+            <label>
+              Nome da Categoria:
+              <input type="text"/>
+
+            </label>
+            <button>
+              Cadastrar
+            </button>
+          </form>
 
           <Link to="/">
             Ir para a Home
