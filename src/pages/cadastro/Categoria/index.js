75ca3dc935cd8ef13420b0a13285a4a23a538e23
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2020-08-02 18:05:32 -0300

    Implemented form

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,62 +1,75 @@
 import React, {useState} from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
+import FormField from '../../../components/FormField';
 
 
 function CadastroCategoria(){
-  const [categorias, setCategorias] = useState(['Teste']);
-  const [nomeDaCategoria, setNomeDaCategoria] =   useState('Filmes');
+  
+  const valoresIniciais = {
+    nome: '',
+    descricao: '',
+    cor: '',
+  }
+  const [values, setValues] =   useState(valoresIniciais);
+  const [categorias, setCategorias] = useState([]);
 
+  function setValue(chave, valor){
+    setValues({
+      ...values,
+      [chave]:valor,
+    })
+  }
+
+  function handleChange(infosDoEvento){
+    const getAttribute = infosDoEvento.target.getAttribute.bind(infosDoEvento.target);
+    const  {value} =  infosDoEvento.target;
+    setValue(
+      getAttribute('name'),
+      value
+    );
+    
+  }
+  
   return(
     <PageDefault>
-        <h1>Página de Cadastro de Categoria: {nomeDaCategoria}</h1>
+        <h1>Página de Cadastro de Categoria: {values.nome}</h1>
         <form 
           onSubmit={function handleSubmit(infosDoEvento){
             infosDoEvento.preventDefault();
             setCategorias([
               ...categorias,
-              nomeDaCategoria
+              values
             ]);
+            setValues(valoresIniciais);
           }}
         >
-          <div>
-            <label>
-              Nome da Categoria:
-              <input 
-                type="text"
-                value={nomeDaCategoria}
-                onChange={function funcaoHandleQueOErroPediu(infosDoEvento){
-                  setNomeDaCategoria(infosDoEvento.target.value);
-                }}
-              />
-            </label>
-          </div>
-          <div>
-            <label>
-              Descrição:
-              <textarea 
-                type="text"
-                value={nomeDaCategoria}
-                onChange={function funcaoHandleQueOErroPediu(infosDoEvento){
-                  setNomeDaCategoria(infosDoEvento.target.value);
-                }}
-              />
-            </label>
-          </div>
           
-          <div>
-            <label>
-              Cor:
-              <input 
-                type="color"
-                value={nomeDaCategoria}
-                onChange={function funcaoHandleQueOErroPediu(infosDoEvento){
-                  setNomeDaCategoria(infosDoEvento.target.value);
-                }}
-              />
-            </label>
-          </div>
-          
+          <FormField
+            label="Nome da Categoria:"
+            type="text"
+            value={values.nome}
+            onChange={handleChange}
+            name="nome"
+
+          />
+
+          <FormField
+            label="Descricão:"
+            type="textArea" 
+            value={values.descricao}
+            onChange={handleChange}
+            name="descricao"
+
+          />
+           <FormField
+            label="Cor:"
+            type="color"
+            value={values.cor}
+            onChange={handleChange}
+            name="cor"
+          />
+         
          
           <button>
             Cadastrar
@@ -64,11 +77,10 @@ function CadastroCategoria(){
         </form>
 
         <ul>
-          
             {categorias.map((categoria, indice) => {
               return (
                 <li key={`${categoria}${indice}`}>
-                  {categoria}
+                  {categoria.nome}
                 </li>
               )
             })}
