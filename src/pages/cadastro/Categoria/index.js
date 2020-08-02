c15bc50eb705834b36146631f9deb48071177ff3
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2020-08-01 22:23:23 -0300

    I started using state

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,28 +1,85 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
 
 
 function CadastroCategoria(){
-    return(
-      <PageDefault>
-          <h1>Página de Cadastro de Categorias</h1>
-          <form>
+  const [categorias, setCategorias] = useState(['Teste']);
+  const [nomeDaCategoria, setNomeDaCategoria] =   useState('Filmes');
+
+  return(
+    <PageDefault>
+        <h1>Página de Cadastro de Categoria: {nomeDaCategoria}</h1>
+        <form 
+          onSubmit={function handleSubmit(infosDoEvento){
+            infosDoEvento.preventDefault();
+            setCategorias([
+              ...categorias,
+              nomeDaCategoria
+            ]);
+          }}
+        >
+          <div>
             <label>
               Nome da Categoria:
-              <input type="text"/>
-
+              <input 
+                type="text"
+                value={nomeDaCategoria}
+                onChange={function funcaoHandleQueOErroPediu(infosDoEvento){
+                  setNomeDaCategoria(infosDoEvento.target.value);
+                }}
+              />
             </label>
-            <button>
-              Cadastrar
-            </button>
-          </form>
+          </div>
+          <div>
+            <label>
+              Descrição:
+              <textarea 
+                type="text"
+                value={nomeDaCategoria}
+                onChange={function funcaoHandleQueOErroPediu(infosDoEvento){
+                  setNomeDaCategoria(infosDoEvento.target.value);
+                }}
+              />
+            </label>
+          </div>
+          
+          <div>
+            <label>
+              Cor:
+              <input 
+                type="color"
+                value={nomeDaCategoria}
+                onChange={function funcaoHandleQueOErroPediu(infosDoEvento){
+                  setNomeDaCategoria(infosDoEvento.target.value);
+                }}
+              />
+            </label>
+          </div>
+          
+         
+          <button>
+            Cadastrar
+          </button>
+        </form>
 
-          <Link to="/">
-            Ir para a Home
-          </Link>
-      </PageDefault>
-    );
+        <ul>
+          
+            {categorias.map((categoria, indice) => {
+              return (
+                <li key={`${categoria}${indice}`}>
+                  {categoria}
+                </li>
+              )
+            })}
+      
+        </ul>
+
+        <Link to="/">
+          Ir para a Home
+        </Link>
+    </PageDefault>
+  );
 }
 
 export default CadastroCategoria;
